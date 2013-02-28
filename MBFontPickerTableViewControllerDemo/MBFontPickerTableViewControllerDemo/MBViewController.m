8b85f4168b3a0ab9cb8380910c6879863de31155
Matthias Bauch <dev@matthiasbauch.com>
2013-02-28 12:05:49 +0100

    demo view controller implementation

--- MBFontPickerTableViewControllerDemo/MBFontPickerTableViewControllerDemo/MBViewController.m
+++ MBFontPickerTableViewControllerDemo/MBFontPickerTableViewControllerDemo/MBViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "MBViewController.h"
+#import "MBFontPickerTableViewController.h"
 
-@interface MBViewController ()
+@interface MBViewController () <MBFontPickerDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *displayNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *fontNameLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *selectFontButton;
+
+- (IBAction)buttonPressed:(UIButton *)sender;
 
 @end
 
@@ -24,6 +32,31 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPressed:(UIButton *)sender {
+    if (sender == self.selectFontButton) {
+        MBFontPickerTableViewController *fontPickerTableView = [[MBFontPickerTableViewController alloc] initWithStyle:UITableViewStylePlain];
+        fontPickerTableView.delegate = self;
+        fontPickerTableView.title = @"Select Font";
+        UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:fontPickerTableView];
+        [self presentViewController:navController animated:YES completion:nil];
+    }
+}
+
+#pragma mark - MBFontPickerDelegate
+
+- (void)fontPicker:(MBFontPickerTableViewController *)fontPicker didSelectFontWithName:(NSString *)fontName {
+    UIFont *font = [UIFont fontWithName:fontName size:15];
+    self.displayNameLabel.font = font;
+    self.displayNameLabel.text = [fontPicker displayNameForFontName:fontName];
+    self.fontNameLabel.font = font;
+    self.fontNameLabel.text = fontName;
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)fontPickerDidCancel:(MBFontPickerTableViewController *)fontPicker {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
